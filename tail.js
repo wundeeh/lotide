61172f1670629f3ae9277ca0f71bc370e13e37b4
wundeeh <jakiriksh@gmail.com>
2022-11-14 20:09:21 +0000

    creates a new array in which it types out all but the first values of the old array

---
+++ tail.js
@@ -0,0 +1,17 @@
+// FUNCTION IMPLEMENTATION
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(i) {
+  let newArray = i.slice(0);
+  return newArray;
+};
+
+// TEST CODE
+
