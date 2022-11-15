ac3a6a6e7b084f95259fe1e407d9d73ef6dba7e5
wndy <114026479+wundeeh@users.noreply.github.com>
2022-11-15 15:01:35 -0600

    Update tail.js

--- tail.js
+++ tail.js
@@ -12,8 +12,3 @@ const tail = function(array) {
   let newArray = array.slice(1);
   console.log(newArray);
 };
-
-// TEST CODE
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
