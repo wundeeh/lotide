22ca5d4868fd6661f238fb13083b8b09fd8b7674
wndy <114026479+wundeeh@users.noreply.github.com>
2022-11-15 15:01:19 -0600

    Update assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -7,8 +7,3 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
-// TEST CODE
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
